7f5b23f9ac12861fb9bef55069453dd909213cb6
aledi99 <alediazuni@gmail.com>
2020-03-25 18:18:23 +0100

    Cambio de menú

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -4,7 +4,8 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   @Output()
